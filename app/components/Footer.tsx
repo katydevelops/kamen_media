da1bfbe6d804117ff2a1613f208c8e9c2eabea81
Katy Senia <katy.develops@gmail.com>
2024-11-03 19:16:05 -0500

    update tailwind imports and add autoprefixer to postccss config file

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,10 +3,10 @@ function Footer() {
     <footer className="bg-gray-900 text-white py-4 text-center border-t border-gray-700 mt-auto">
       <div className="container mx-auto">
         <p>&copy; {new Date().getFullYear()} Andrew Kamen - DJ Services in Pittsburgh</p>
-        <div className="flex justify-center space-x-4 mt-2">
-          <a href="#" className="hover:text-gray-400">Twitter</a>
-          <a href="#" className="hover:text-gray-400">Instagram</a>
-          <a href="#" className="hover:text-gray-400">Facebook</a>
+        <div className="flex justify-center gap-x-6 mt-2"> {/* Adjusted space */}
+          <a href="#" className="text-white hover:text-gray-400 no-underline">Twitter</a>
+          <a href="#" className="text-white hover:text-gray-400 no-underline">Instagram</a>
+          <a href="#" className="text-white hover:text-gray-400 no-underline">Facebook</a>
         </div>
       </div>
     </footer>
